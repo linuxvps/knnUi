d85966c14ea7fae8ad72f01e4c3283fce04bacd1
linuxvps <linux.vps.vpn@gmail.com>
2024-03-15 16:47:09 +0330

    change component to component directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CardComponent} from "./card/card.component";
+import {CardComponent} from "./component/card/card.component";
 
 @Component({
   selector: 'app-root',
