075026112bc6b66f4e68035aad876e0e7432d36a
Linux.vps.vpn <Linux.vps.vpn@gmail.com>
2024-03-21 01:07:05 -0700

    add sign in ui code and delete app root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {CardComponent} from "./component/card/card.component";
 import {ContactUsComponent} from "./component/contact-us/contact-us.component";
+import {SigninComponent} from "./component/signin/signin.component";
 
 export const routes: Routes = [
   {path: 'card', component: CardComponent},
-  {path: 'contact-us', component: ContactUsComponent}
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'sign-in', component: SigninComponent}
 ];
