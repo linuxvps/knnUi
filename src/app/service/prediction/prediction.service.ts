715866a2852d54865f77b8ad0b036f46c04d4818
alamshenas <linux.vps.vpn@gmail.com>
2024-09-30 12:39:01 +0330

    aaa

--- src/app/service/prediction/prediction.service.ts
+++ src/app/service/prediction/prediction.service.ts
@@ -16,7 +16,13 @@ export class PredictionService {
 
   constructor(private http: HttpClient) {}
 
-  getPredictions() {
+  getPredictions()
+  {
+    console.log(environment.rootUrl + Constants.PREDICTIONS_API_URL)
+    console.log(this.http.get(
+      environment.rootUrl + Constants.PREDICTIONS_API_URL, {observe: 'response'}
+    ))
+
     return this.http.get(
       environment.rootUrl + Constants.PREDICTIONS_API_URL, {observe: 'response'}
     )
