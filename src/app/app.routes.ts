44c8778d931df65f339c31e74c0a9ed22573e1ba
NIMA <linux.vps.vpn@gmail.com>
2024-10-03 16:40:37 -0700

    add input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,5 +11,5 @@ export const routes: Routes = [
   {path: 'contact-us', component: ContactUsComponent},
   {path: 'sign-up', component: SignupComponent},
   {path: 'sign-in', component: SigninComponent},
-  {path: '/prediction', component: PredictionViewComponent}
+  {path: 'prediction', component: PredictionViewComponent}
 ];
